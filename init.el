c2fd18c427df685931162259016d2ee0f70dde83
Yusuke Yoshimi <yoshimi@fun.bio.keio.ac.jp>
2017-06-17 03:06:51 +0900

    create my init.el for emacs

---
+++ init.el
@@ -0,0 +1,66 @@
+;; 環境を日本語、UTF-8にする
+(set-locale-environment nil)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; 複数ウィンドウを禁止する
+(setq ns-pop-up-frames nil)
+
+;; メニューバーを消す
+(menu-bar-mode -1)
+
+;; ツールバーを消す
+(tool-bar-mode -1)
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+(set-face-attribute 'linum nil :foreground "#08E" :height 0.9)
+(setq linum-format "%3d  ")
+
+;; カーソルの点滅をやめる
+(blink-cursor-mode 0)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; ウィンドウ内に収まらないときだけ、カッコ内も光らせる
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
+
+;; スクロールは１行ごとに
+(setq scroll-conservatively 1)
+
+;; C-kで行全体を削除する
+(setq kill-whole-line t)
+
+;;; dired設定
+(require 'dired-x)
+
+;; "yes or no" の選択を "y or n" にする
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; beep音を消す
+(defun my-bell-function ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'my-bell-function)
